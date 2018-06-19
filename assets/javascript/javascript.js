00a546a0309f4ad6f598337945ba4f80fc7e3fab
Dan <dpedersen84@gmail.com>
2018-06-19 18:06:46 -0500

    github pages updated to reflect portfolio

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+    M.AutoInit();
+
+    console.log("hello");
+    // VARIABLES
+    
+    // FUNCTIONS
+
+    // EVENTS
+    $("#portfolio").on("click", function (event) {
+      console.log("Hello");
+    });
+
+    $("#contact").on("click", function (event) {
+      console.log("Hello");
+    });
+
+    $("#index").on("click", function (event) {
+      console.log("Hello");
+    });
+
+    $("#viewMiles").on("click", function (event) {
+      console.log("Hello");
+      window.location.href = "https://milesdriver-project2.herokuapp.com/";
+    });
+
+    $("#viewFriendFinder").on("click", function (event) {
+      console.log("Hello");
+      window.location.href = "https://glacial-scrubland-30755.herokuapp.com/";
+    });
+
+    $("#viewBamazon").on("click", function (event) {
+      console.log("Hello");
+      // window.location.href = "https://milesdriver-project2.herokuapp.com/";
+    });
+
+    $("#viewBurgerMunch").on("click", function (event) {
+      console.log("Hello");
+      window.location.href = "https://tranquil-headland-65629.herokuapp.com/";
+    });
+    
+  });
+      
